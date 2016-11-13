4f6cae587cd0805e383e9e927c014eb2104d013a
ayako1967 <takuya_kctn1414@tbz.t-com.ne.jp>
2016-11-13 16:20:05 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/takuya/okitsu/taskapplication/MainActivity.java
@@ -0,0 +1,72 @@
+package jp.techacademy.takuya.okitsu.taskapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private TaskAdapter mTaskAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //ListViewの設定
+        mTaskAdapter = new TaskAdapter(MainActivity.this);
+        mListView = (ListView) findViewById(R.id.listView1);
+
+        //ListViewをタップ
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //入力・編集画面へ
+
+            }
+        });
+        //ListViewを長押し
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                //タスク削除
+                return true;
+            }
+        });
+        reloadListView();
+    }
+
+    private void reloadListView(){
+
+        //Taskクラスに変更する
+        ArrayList<String> taskArrayList = new ArrayList<>();
+
+        taskArrayList.add("aaa");
+        taskArrayList.add("bbb");
+        taskArrayList.add("ccc");
+
+        mTaskAdapter.setTaskArrayList(taskArrayList);
+        mListView.setAdapter(mTaskAdapter);
+        mTaskAdapter.notifyDataSetChanged();
+    }
+
+
+}
